5e1f990216c1a6a7b5830252c30310f6218fed65
olkku45 <olli.tervonen05@gmail.com>
2026-01-02 21:36:12 +0200

    update files

--- src/CodeGen.zig
+++ src/CodeGen.zig
@@ -1,5 +1,38 @@
 const std = @import("std");
+const Instruction = @import("IRGenerator.zig").Instruction;
+
+fn createAsmFile() !std.fs.File {
+    const file = try std.fs.cwd().createFile("test.s", .{}); // assuming unix
+    return file;
+}
 
 pub const CodeGen = struct {
-    allocator: std.mem.Allocator,  
+    allocator: std.mem.Allocator,
+    instructions: []Instruction,
+    file: std.fs.File,
+
+    pub fn init(instructions: []Instruction, allocator: std.mem.Allocator) CodeGen {
+        return CodeGen{
+            .instructions = instructions,
+            .allocator = allocator,
+            .file = createAsmFile(),
+        };
+    }
+
+    pub fn deinit(self: *CodeGen) void {
+        self.file.close();
+    }
+
+    pub fn writeAsm(self: *CodeGen) !void {
+        for (self.instructions) |instruction| {
+            switch (instruction) {
+                .function => {},
+                .allocation => {},
+                .expression => {},
+                .ret => {},
+                .assignment => {},
+                .func_end => {},
+            }
+        }
+    }
 };
